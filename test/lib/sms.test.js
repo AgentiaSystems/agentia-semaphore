f2b3afd47659276780008ff887b65e121f1982d3
Johnny Estilles <johnny.estilles@agentia.asia>
2015-05-26 16:41:25 +0800

    fix lit in test

--- test/lib/sms.test.js
+++ test/lib/sms.test.js
@@ -1,65 +1,80 @@
-var _ = require('lodash'),
-	chai = require('chai'),
-	expect = chai.expect,
-	sinon = require('sinon'),
-	sinonChai = require('sinon-chai'),
-	semaphore = require('../../');
+'use strict';
+
+var chai = require('chai');
+var expect = chai.expect;
+var sinon = require('sinon');
+var sinonChai = require('sinon-chai');
+var semaphore = require('../../');
 
 chai.use(sinonChai);
 
 describe('.sms()', function () {
-	var callback = sinon.stub();
+  var callback = sinon.stub();
 
-	before(function () {
-		sinon.stub(semaphore, 'api');
-	});
+  before(function () {
+    sinon.stub(semaphore, 'api');
+  });
 
-	after(function() {
-		semaphore.api.restore();
-	})
+  after(function() {
+    semaphore.api.restore();
+  });
 
-	afterEach(function() {
-		semaphore.api.reset();
-	});
+  afterEach(function() {
+    semaphore.api.reset();
+  });
 
-	it('should call api with number, message and callback', function() {
-		var number = '1234567890', message = 'this is a test';
+  it('should call api with number, message and callback', function() {
+    var number = '1234567890', message = 'this is a test';
 
-		semaphore.sms(number, message, callback);
-		expect(semaphore.api).to.have.been.called.once;
-		expect(semaphore.api.getCall(0).args[0]).to.equal('sms');
-		expect(semaphore.api.getCall(0).args[1]).to.deep.equal({ number: number, message: message	});
-		expect(semaphore.api.getCall(0).args[2]).to.equal(callback);
-	});
+    semaphore.sms(number, message, callback);
+    expect(semaphore.api).to.have.been.called.once;
+    expect(semaphore.api.getCall(0).args[0]).to.equal('sms');
+    expect(semaphore.api.getCall(0).args[1]).to.deep.equal({
+			number: number,
+			message: message
+		});
+    expect(semaphore.api.getCall(0).args[2]).to.equal(callback);
+  });
 
-	it('should call api with number and message, but no callback', function() {
-		var number = '1234567890', message = 'this is a test';
+  it('should call api with number and message, but no callback', function() {
+    var number = '1234567890', message = 'this is a test';
 
-		semaphore.sms(number, message);
-		expect(semaphore.api).to.have.been.called.once;
-		expect(semaphore.api.getCall(0).args[0]).to.equal('sms');
-		expect(semaphore.api.getCall(0).args[1]).to.deep.equal({ number: number, message: message	});
-		expect(semaphore.api.getCall(0).args[2]).to.equal(undefined);
-	});
+    semaphore.sms(number, message);
+    expect(semaphore.api).to.have.been.called.once;
+    expect(semaphore.api.getCall(0).args[0]).to.equal('sms');
+    expect(semaphore.api.getCall(0).args[1]).to.deep.equal({
+			number: number,
+			message: message
+		});
+    expect(semaphore.api.getCall(0).args[2]).to.equal(undefined);
+  });
 
-	it('should call api with number, message, from and callback', function() {
-		var number = '1234567890', message = 'this is a test', from = 'origin';
+  it('should call api with number, message, from and callback', function() {
+    var number = '1234567890', message = 'this is a test', from = 'origin';
 
-		semaphore.sms(number, message, from, callback);
-		expect(semaphore.api).to.have.been.called.once;
-		expect(semaphore.api.getCall(0).args[0]).to.equal('sms');
-		expect(semaphore.api.getCall(0).args[1]).to.deep.equal({ number: number, message: message, from: from	});
-		expect(semaphore.api.getCall(0).args[2]).to.equal(callback);
-	});
+    semaphore.sms(number, message, from, callback);
+    expect(semaphore.api).to.have.been.called.once;
+    expect(semaphore.api.getCall(0).args[0]).to.equal('sms');
+    expect(semaphore.api.getCall(0).args[1]).to.deep.equal({
+			number: number,
+			message: message,
+			from: from
+		});
+    expect(semaphore.api.getCall(0).args[2]).to.equal(callback);
+  });
 
-	it('should call api with number, message and from, but no callback', function() {
-		var number = '1234567890', message = 'this is a test', from = 'origin';
+  it('should call api with number/message/from, but no callback', function() {
+    var number = '1234567890', message = 'this is a test', from = 'origin';
 
-		semaphore.sms(number, message, from);
-		expect(semaphore.api).to.have.been.called.once;
-		expect(semaphore.api.getCall(0).args[0]).to.equal('sms');
-		expect(semaphore.api.getCall(0).args[1]).to.deep.equal({ number: number, message: message, from: from	});
-		expect(semaphore.api.getCall(0).args[2]).to.equal(undefined);
-	});
+    semaphore.sms(number, message, from);
+    expect(semaphore.api).to.have.been.called.once;
+    expect(semaphore.api.getCall(0).args[0]).to.equal('sms');
+    expect(semaphore.api.getCall(0).args[1]).to.deep.equal({
+			number: number,
+			message: message,
+			from: from
+		});
+    expect(semaphore.api.getCall(0).args[2]).to.equal(undefined);
+  });
 
 });
